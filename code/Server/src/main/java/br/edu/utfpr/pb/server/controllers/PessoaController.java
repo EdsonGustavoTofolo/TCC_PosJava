61c2e897c8faa8ccd5b5ecbb89c39b25a0d037f8
Guto Tofolo <Guto Tofolo>
2017-03-10 23:02:15 -0300

    Inserido estrutura do client utilizando angular 2 com bootstrap 4

--- code/Server/src/main/java/br/edu/utfpr/pb/server/controllers/PessoaController.java
+++ code/Server/src/main/java/br/edu/utfpr/pb/server/controllers/PessoaController.java
@@ -5,7 +5,6 @@ import br.edu.utfpr.pb.server.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
