801c59e4ab2373377041da52aa4021887e42df7f
Guto Tofolo <Guto Tofolo>
2017-03-27 22:58:20 -0300

    campos no fonte requerimento

--- code/Client/angular-client/src/app/views/requerimento/requerimento.component.ts
+++ code/Client/angular-client/src/app/views/requerimento/requerimento.component.ts
@@ -10,6 +10,18 @@ export class RequerimentoComponent implements OnInit {
   motivos = motivosRequerimentos;
   motivoSelected: {key: 0, value: ''};
 
+  //Se for selecionada a opção 9 - 2ª chamada da prova
+  segundaChamadaProva: {disciplina: '', professor: '', data: Date};
+
+  //Se for selecionada a opção "5 - Cancelamento das Disciplinas" ou
+  //                          "15 - Matrícula nas disciplinas" ou
+  //                          "17 - Planos Ensinos/Ementas das disciplinas"
+  disciplinas: string[];
+
+  //Se for selecionada a opção "16 - Mudança para as turmas"
+  turmas: string[];
+
+  //Se for selecionado a opção "20 - Outros", apenas abre um campo texto para digitar o que quiser
   constructor() { }
 
   ngOnInit() {
