dbaf56b6d688398eb0acfccdc77dd361b8db2189
Edson Gustavo Tofolo <edsontofolo@gmail.com>
2017-10-10 19:37:51 -0300

    excluida pasta do angular

--- code_with_angular2/Client/angular-client/src/app/models/convalidacao-item.ts
+++
@@ -1,6 +0,0 @@
-export class ConvalidacaoItem {
-  constructor(public id?: number,
-              public codigo?:string,
-              public disciplina?: string,
-              public disciplinas?: string[]) {}
-}
--- code_with_angular2/Client/angular-client/src/app/models/convalidacao.ts
+++
@@ -1,9 +0,0 @@
-import {ConvalidacaoItem} from "./convalidacao-item";
-export class Convalidacao {
-  constructor(public id?: number,
-              public codigo?: number,
-              public numero?: number,
-              public aluno?: string,
-              public coordenacao?: string,
-              public items?: ConvalidacaoItem[]) {}
-}
